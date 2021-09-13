d010bb960fced0083cf59850f3afbc15b97f524a
vanhaeckegab <vanhaeckegab@urbandaleschools.com>
2021-09-13 09:50:48 -0500

    updated ch 2

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,21 +1,24 @@
 '''
-Sign your name:________________
+Sign your name:Gabe Van Haecke
 
 1.) How do you enter a single line comment in a program? Give an example.
 
+# This is how you make a comment
 
-2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
+2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 =2.5
+b//a                =2
+b**a                =25
+b%a                 =1
+a+B                 =Traceback (most recent call last):      Error= it doesn't know what to do with a capital b
+                    File "<input>", line 1, in <module>
+                    NameError: name 'B' is not defined
+type(42)            =<class 'int'>
+type(42.0)          =<class 'float'>
+type("C3PO")        =<class 'str'>
+type(True)          =<class 'bool'>
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,22 +29,24 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = <class 'float'>
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3*(x + y)
 a
 
+need a * after the 3
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = (x+y+z)/3
 ave
 
+need () around x+y+z
 
 '''
